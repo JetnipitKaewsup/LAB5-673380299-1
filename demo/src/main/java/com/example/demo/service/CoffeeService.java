98d28459518292cea0461e86d82cdaa1608d487d
JetnipitKaewsup <Jetnipit.Kaewsup@gmail.com>
2026-07-31 21:21:09 +0700

    update

--- demo/src/main/java/com/example/demo/service/CoffeeService.java
+++ demo/src/main/java/com/example/demo/service/CoffeeService.java
@@ -1,57 +1,69 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Coffee;
-import com.example.demo.repository.CoffeeRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CoffeeService {
 
-    private final CoffeeRepository repository;
+    private List<Coffee> coffeeList = new ArrayList<>();
 
-    public CoffeeService(CoffeeRepository repository) {
-        this.repository = repository;
+    // Constructor
+    public CoffeeService() {
+        coffeeList.add(new Coffee(1, "Espresso", 55));
+        coffeeList.add(new Coffee(2, "Latte", 70));
+        coffeeList.add(new Coffee(3, "Mocha", 80));
     }
 
+    // GET
     public List<Coffee> getAllCoffee() {
-        return repository.findAll();
+        return coffeeList;
     }
 
+    // GET by id
     public Coffee getCoffee(int id) {
-        return repository.findById(id);
+        for (Coffee coffee : coffeeList) {
+            if (coffee.getId() == id) {
+                return coffee;
+            }
+        }
+        return null;
     }
 
+    // POST
     public void addCoffee(Coffee coffee) {
-        repository.save(coffee);
+        coffeeList.add(coffee);
     }
 
-    public boolean updateCoffee(int id, Coffee coffee) {
+    // PUT
+    public boolean updateCoffee(int id, Coffee newCoffee) {
 
-        Coffee oldCoffee = repository.findById(id);
+        Coffee oldCoffee = getCoffee(id);
 
-        if (oldCoffee == null)
+        if (oldCoffee == null) {
             return false;
+        }
 
-        oldCoffee.setName(coffee.getName());
-        oldCoffee.setPrice(coffee.getPrice());
+        oldCoffee.setName(newCoffee.getName());
+        oldCoffee.setPrice(newCoffee.getPrice());
 
         return true;
-
     }
 
+    // DELETE
     public boolean deleteCoffee(int id) {
 
-        Coffee coffee = repository.findById(id);
+        Coffee coffee = getCoffee(id);
 
-        if (coffee == null)
+        if (coffee == null) {
             return false;
+        }
 
-        repository.delete(id);
+        coffeeList.remove(coffee);
 
         return true;
-
     }
-
 }
